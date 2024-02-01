292a573314303ada8e5786f6026bbc2d58ac769e
James Simmonds <jameshsimmonds@gmail.com>
2024-02-01 16:47:05 +0000

    basic layout and simple styles applied

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,24 @@
+import "../pages/LandingPage.scss";
+import Logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+
 function LandingPage() {
   console.log(window.location.pathname);
   return (
     <>
-      <h1>Landing page.....under construction</h1>
+      <section className="landing">
+        <div className="landing-container">
+          <div className="landing-container_logo">
+            <img className="landing-container_logo" src={Logo} />
+          </div>
+
+          <div className="landing_button">
+            <Link to="/home">
+              <img />
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/SantanderBikeStations.js
+++ src/pages/SantanderBikeStations.js
@@ -1,9 +1,11 @@
 import SantanderFinder from "../components/SantanderFinder";
 import ButtonsNavBar from "../components/ButtonsNavBar";
+import Header from "../components/Header";
 
 function SantanderBikeStations() {
   return (
     <>
+      <Header />
       <SantanderFinder />
       <ButtonsNavBar />
     </>
